5b2c281c99c3aa6e6e300730a103e3683d9a34de
alexyang256 <alexyang256@berkeley.edu>
2018-11-17 13:51:25 -0800

    Added pathfinding algorithm

---
+++ maze_utils.py
@@ -0,0 +1,103 @@
+# Search
+import collections
+import numpy as np
+from enum import Enum
+
+class Direction(Enum):
+	NOWHERE = 0
+	LEFT = 63
+	UP = 127
+	RIGHT = 191
+	DOWN = 255
+
+	def opposite(self):
+		if self == Direction.LEFT:
+			return Direction.RIGHT
+		elif self == Direction.RIGHT:
+			return Direction.LEFT
+		elif self == Direction.UP:
+			return Direction.DOWN
+		elif self == Direction.DOWN:
+			return Direction.UP
+		else:
+			return Direction.NOWHERE
+
+# Returns the valid neighboring grid squares
+# paired with the DIRECTION to RETURN to the first square
+def neighbors(point, grid):
+	result = []
+	y, x, height, width = point[0], point[1], grid.shape[0], grid.shape[1]
+	if y > 0:
+		result.append(((y-1, x), Direction.DOWN))
+	if y < height-1:
+		result.append(((y+1, x), Direction.UP))
+	if x > 0:
+		result.append(((y, x-1), Direction.RIGHT))
+	if x < width-1:
+		result.append(((y, x+1), Direction.LEFT))
+	return result
+
+def compute_wall_distances(grid, wall_distances):
+	visited, queue = set(), collections.deque([])
+	walls = grid.nonzero()
+	for i in range(len(walls[0])):
+		wall = (walls[0][i], walls[1][i])
+		visited.add(wall)
+		wall_distances[wall[0]][wall[1]] = 0
+		queue.append(wall)
+	while queue:
+		vertex = queue.popleft()
+		for adjacent, _ in neighbors(vertex, grid):
+			if adjacent not in visited:
+				visited.add(adjacent)
+				queue.append(adjacent)
+				wall_distances[adjacent[0]][adjacent[1]] = wall_distances[vertex[0]][vertex[1]] + 1
+
+# Performs bfs on grid
+# At the end, directions will be populated with elements in [0, 4]
+# where 0 = this square is the destination
+# 1 = go left to reach destination
+# 2 = go up
+# 3 = go right
+# 4 = go down
+def breadth_first_search(grid, dest, directions, distances): 
+    visited, queue = set(), collections.deque([dest])
+    directions[dest[0]][dest[1]] = Direction.NOWHERE.value
+    distances[dest[0]][dest[1]] = 0
+    visited.add(dest)
+    while queue: 
+        vertex = queue.popleft()
+        for adjacent, direction_code in neighbors(vertex, grid):
+            if adjacent not in visited and not grid[adjacent[0]][adjacent[1]]:
+                visited.add(adjacent)
+                queue.append(adjacent)
+                directions[adjacent[0]][adjacent[1]] = direction_code.value
+                distances[adjacent[0]][adjacent[1]] = distances[vertex[0]][vertex[1]] + 1
+
+def create_direction_matrix(grid, distances, wall_distances):
+	result = np.zeros(shape=grid.shape, dtype="uint8")
+	for i in range(len(result)):
+		for j in range(len(result[0])):
+			if not grid[i][j] and distances[i][j] != 0:
+				candidates = neighbors((i, j), grid)
+				best_direction = min(candidates, key=lambda c: (distances[c[0][0]][c[0][1]], -wall_distances[c[0][0]][c[0][1]]))
+				result[i][j] = best_direction[1].opposite().value
+	return result
+
+def find_path(source, directions):
+	path = []
+	curr = source
+	while directions[curr[0]][curr[1]] != Direction.NOWHERE.value:
+		direction = directions[curr[0]][curr[1]]
+		path.append((direction, curr))
+		if direction == Direction.LEFT.value:
+			curr = (curr[0], curr[1]-1)
+		elif direction == Direction.UP.value:
+			curr = (curr[0]-1, curr[1])
+		elif direction == Direction.RIGHT.value:
+			curr = (curr[0], curr[1]+1)
+		elif direction == Direction.DOWN.value:
+			curr = (curr[0]+1, curr[1])
+
+	return path
+
--- process_maze_image.py
+++ process_maze_image.py
@@ -1,10 +1,12 @@
 # import the necessary packages
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 import argparse
 import cv2
 from sklearn.cluster import KMeans
 from projection import projection
 from image_utils import detect_corners, gridify, overlay_visualize
+from maze_utils import breadth_first_search, find_path, compute_wall_distances, create_direction_matrix
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -17,6 +19,7 @@ lower =	np.array([140, 140, 0], dtype="uint8")
 upper = np.array([230, 230, 60], dtype = "uint8")
 
 # Desired resolution of projected image
+# 91.3, 123.6 is REAL LIFE CM measurement
 width, height = 130 * 6, 91 * 6
 
 # Desired resolution of maze grid
@@ -33,3 +36,16 @@ maze_grid = gridify(projected, grid_width, grid_height, wall_threshold)
 visualized_maze_grid = overlay_visualize(projected, maze_grid)
 
 cv2.imwrite("projected_walls_grid_final.png", visualized_maze_grid)
+
+bools = maze_grid.astype(bool)
+distances_from_walls = np.empty(shape=maze_grid.shape, dtype="uint8")
+directions_simple = np.zeros(shape=maze_grid.shape, dtype="uint8")
+distances = np.full(maze_grid.shape, np.inf)
+
+compute_wall_distances(bools, distances_from_walls)
+breadth_first_search(bools, (40, 58), directions_simple, distances)
+directions_smart = create_direction_matrix(bools, distances, distances_from_walls)
+
+path = find_path((1, 1), directions_smart)
+for direc, square in path:
+	print(square)
