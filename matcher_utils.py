ace3bffc7de4ae939618ec9c52a8cc39d5e819c8
alexyang256 <alexyang256@berkeley.edu>
2018-12-06 16:38:55 -0800

    Fixed angle detection, changed color detection to work for micromouse maze

--- matcher_utils.py
+++ matcher_utils.py
@@ -1,41 +1,82 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import math
 
-img1 = cv2.imread('maze_images/raw_robot.png',0)          # queryImage
-img2 = cv2.imread('maze_images/maze4.jpg',0) # trainImage
+MIN_MATCH_COUNT = 10
 
-# Initiate SIFT detector
-orb = cv2.ORB_create()
+# Return angle in degrees that the robot has rotated from facing LEFT in the maze
+# as well as the top left and bottom right of the bounding box containing it
+def find_robot_angle(robot_img=cv2.imread("maze_images/raw_robot2.png"), maze_img=cv2.imread("raw_projected.png")):
+	img1 = cv2.cvtColor(robot_img, cv2.COLOR_BGR2GRAY)	# queryImage
+	img2 = cv2.cvtColor(maze_img, cv2.COLOR_BGR2GRAY)		# trainImage
+	# Initiate SIFT detector
+	sift = cv2.xfeatures2d.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
+	# find the keypoints and descriptors with SIFT
+	kp1, des1 = sift.detectAndCompute(img1,None)
+	kp2, des2 = sift.detectAndCompute(img2,None)
 
-# create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+	FLANN_INDEX_KDTREE = 0
+	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+	search_params = dict(checks = 50)
 
-# Match descriptors.
-matches = bf.match(des1,des2)
+	flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+	matches = flann.knnMatch(des1,des2,k=2)
+
+	# store all the good matches as per Lowe's ratio test.
+	good = []
+	for m,n in matches:
+	    if m.distance < 0.7*n.distance:
+	        good.append(m)
+
+	src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+	dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+	print(M)
+	matchesMask = mask.ravel().tolist()
 
 
-src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
-dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+	h,w = img1.shape
+	pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+	dst = cv2.perspectiveTransform(pts,M)
 
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-matchesMask = mask.ravel().tolist()
+	# img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+	# draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+ #                   singlePointColor = None,
+ #                   matchesMask = matchesMask, # draw only inliers
+ #                   flags = 2)
 
-h,w = img1.shape
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
+	# img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
-img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+	# plt.imshow(img3, 'gray'),plt.show()
 
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
+	# Transformed upper left, transformed upper right.
+	p2, q2 = dst[0][0], dst[3][0]
+	print(p2, q2)
+	dy = q2[1] - p2[1]
+	dx = q2[0] - p2[0]
 
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,**draw_params)
+	dst = dst.reshape(4,2).T
+	if dx > 0:
+		angle = -1 * 180 / math.pi * math.atan(dy/dx)
+	else:
+		angle = 180 - 180 / math.pi * math.atan(dy/dx)
 
-plt.imshow(img3, 'gray'),plt.show()
+	min_x = np.min(dst[0])
+	min_y = np.min(dst[1])
+	max_x = np.max(dst[0])
+	max_y = np.max(dst[1])
+	top_left = (min_x, min_y)
+	bottom_right = (max_x, max_y)
+	return top_left, bottom_right, clip_to_range(angle)
+
+# Clip the angle to [-180, 180]
+def clip_to_range(angle):
+	if -180 <= angle <= 180:
+		return angle
+	elif angle < -180:
+		return 360 + angle
+	else:
+		return -360 + angle
--- process_maze_image.py
+++ process_maze_image.py
@@ -6,11 +6,12 @@ import argparse
 import cv2
 # import bluetooth
 import time
-from sklearn.cluster import KMeans
 from projection import projection
 from image_utils import detect_corners, gridify, gridify2, overlay_visualize, pad_walls
 from maze_utils import breadth_first_search, find_path, compute_wall_distances, create_direction_matrix, Direction, print_path
+from matcher_utils import find_robot_angle
 
+start = time.clock()
 maze_image_filepath = "maze_images/current_maze.jpg"
 
 # construct the argument parse and parse the arguments
@@ -22,13 +23,14 @@ image = cv2.imread(args["image"])
 robot = cv2.imread(args["robot"]) # It's facing left
 
 # Threshold values for maze corner detection (green)
-lower =	np.array([140, 140, 0], dtype="uint8")
-upper = np.array([230, 230, 60], dtype = "uint8")
+lower =	np.array([130, 50, 70], dtype="uint8")
+upper = np.array([200, 90, 120], dtype = "uint8")
 
 # Desired resolution of projected image
 # 91.3, 123.6 is REAL LIFE CM measurement
 width, height = 130 * 6, 91 * 6
 width, height = 1236, 913
+width, height = 687, 461
 
 # Desired resolution of maze grid
 grid_width, grid_height = width // 13, height // 13
@@ -36,73 +38,56 @@ grid_width, grid_height = width // 13, height // 13
 # Desired downscale factor from projected image to maze grid
 # Units of MILLIMETERS SQUARED PER GRID SQUARE or 
 # MILLIMETERS PER GRID SQUARE LENGTH
-desired_downscale_factor = 20
+desired_downscale_factor = 8
 
-# Threshold when processing raw projected maze image
+# Threshold when processing grayscaled projected maze image
 wall_threshold = 210
 
-
-
-
-
-
+start_project_time = time.clock()
 sorted_centers = detect_corners(image, lower, upper)
 
 projected = projection(args["image"], height, width, sorted_centers.astype(np.float32))
 padded = pad_walls(projected, desired_downscale_factor)
+end_project_time = time.clock()
+cv2.imwrite("raw_projected.png", padded)
+
 
 # Find robot within padded
-
-# Get all four orientations
-rows, cols = robot.shape[:2]
-rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), -90, 1)
-robot_left = robot
-robot_down = cv2.warpAffine(robot_left, rotation_matrix, (cols, rows))
-robot_right = cv2.warpAffine(robot_down, rotation_matrix, (cols, rows))
-robot_up = cv2.warpAffine(robot_right, rotation_matrix, (cols, rows))
-
-robot_orientations = [robot_left, robot_down, robot_right, robot_up]
-template_match_results = map(lambda orientation: cv2.matchTemplate(orientation, padded, cv2.TM_SQDIFF), robot_orientations)
-template_match_results = enumerate(list(map(lambda res: cv2.minMaxLoc(res), template_match_results)))
-best_result, best_i = None, 47
-for i, res in template_match_results:
-	if best_i == 47 or res[0] < best_result[0]:
-		best_result = res
-		best_i = i
-
-initial_direction = Direction((best_i + 1) * 63)
-min_val, max_val, min_loc, max_loc = best_result
-top_left = min_loc
-bottom_right = (min_loc[0] + robot.shape[1], min_loc[1] + robot.shape[0])
+start_findrobot_time = time.clock()
+top_left, bottom_right, angle = find_robot_angle(robot, padded)
+end_findrobot_time = time.clock()
 robot_center = ((top_left[0] + bottom_right[0])//2, (top_left[1] + bottom_right[1])//2)
 robot_center_gridsquare = (robot_center[1] // desired_downscale_factor, robot_center[0] // desired_downscale_factor)
 radius = ceil((bottom_right[0] - top_left[0]) / 2 / desired_downscale_factor)
 
-cv2.imwrite("raw_projected.png", padded)
-
+start_gridify_time = time.clock()
 maze_grid = gridify2(padded, desired_downscale_factor, wall_threshold, top_left, bottom_right)
-visualized_maze_grid = overlay_visualize(padded, maze_grid)
-
-cv2.imwrite("projected_walls_grid_final.png", visualized_maze_grid)
+end_gridify_time = time.clock()
+# cv2.imwrite("temp.png", maze_grid)
 
 bools = maze_grid.astype(bool)
 distances_from_walls = np.empty(shape=maze_grid.shape, dtype="uint8")
 directions_simple = np.zeros(shape=maze_grid.shape, dtype="uint8")
 distances = np.full(maze_grid.shape, np.inf)
 
+start_bfs_time = time.clock()
 compute_wall_distances(bools, distances_from_walls)
-cv2.imwrite("wall_distances_2.png", distances_from_walls * 28)
-bools_buffered = distances_from_walls <= radius
-breadth_first_search(bools_buffered, (6, 6), directions_simple, distances)
-directions_smart = create_direction_matrix(bools_buffered, distances, distances_from_walls)
 
+bools_buffered = distances_from_walls <= radius
+breadth_first_search(bools_buffered, (50, 7), directions_simple, distances)
+directions_smart = create_direction_matrix(bools_buffered, distances, distances_from_walls)
+end_bfs_time = time.clock()
+start_findpath_time = time.clock()
+path = find_path(robot_center_gridsquare, directions_smart, angle)
+end_findpath_time = time.clock()
+
+visualized_maze_grid = overlay_visualize(padded, bools_buffered.astype("uint8"))
+cv2.imwrite("wall_distances_2.png", distances_from_walls * 28)
+cv2.imwrite("projected_walls_grid_final.png", visualized_maze_grid)
 cv2.imwrite("distances2.png", distances)
 cv2.imwrite("magic2.png", directions_smart)
 
-print(initial_direction)
-path = find_path(robot_center_gridsquare, directions_smart, initial_direction)
-
-path_modified = [str(el) if type(el) == int else str(el.value) for el in path]
+# path_modified = [str(el) if type(el) == int else str(el.value) for el in path]
  
 # bd_addr = "00:14:03:06:75:C2" 
 # port = 1
@@ -119,3 +104,8 @@ path_modified = [str(el) if type(el) == int else str(el.value) for el in path]
 # sock.close()
 
 print_path(path)
+print("Projecion and corner detection: " + str(end_project_time - start_project_time))
+print("Gridify: " + str(end_gridify_time - start_gridify_time))
+print("Robot finding: " + str(end_findrobot_time - start_findrobot_time))
+print("BFS to find distances from walls, distances from every square, directions from every square: " + str(end_bfs_time - start_bfs_time))
+print("Constructing path: " + str(end_findpath_time - start_findpath_time))
