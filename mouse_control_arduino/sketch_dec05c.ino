07976bd2c6a41791a224813917f527d8ca4211e8
Khanh Dang <khanhdang@airbears2-10-142-33-153.airbears2.1918.berkeley.edu>
2018-12-10 19:00:15 -0800

    change into parseFloat()

--- mouse_control_arduino/sketch_dec05c.ino
+++ mouse_control_arduino/sketch_dec05c.ino
@@ -30,15 +30,15 @@ float u_right = 0;
 float u_left = 0;
 
 //distance, timing
-float dist = 0;
-float angle = 0;
+float dist = 0.0;
+float angle = 0.0;
 unsigned long prev_time;
 unsigned long curr_time;
 unsigned long start_time;
 unsigned long end_time;
-int target = 0;
-int target_dist = 0;
-int input = 0;
+float target = 0.0;
+float target_dist = 0.0;
+float input = 0.0;
 
 // flag for whether receive angle or distance
 boolean recv = true;
@@ -59,36 +59,24 @@ void loop() {
 
   // if receive input from bluetooth, execute commands
   if (Serial.available()) {
-      input = Serial.parseInt();
-      if (input == 0) {
-//          dist = 0;
-//          angle = 0;
-//          count = 0;   
-//          flag = true;
-          target= 0;
-          target_dist = 0;
+      input = Serial.parseFloat(); // receiving input from bluetooth
+      if (input == 0.0) {          // when input is 0, no action is taken
+          target = 0.0;
+          target_dist = 0.0;
           Serial.println("input = 0");
           while (Serial.available()) {
             Serial.read();
             Serial.flush();
           }
     } else {
-        if (recv) {
-          target_dist = 0;
+        if (recv) { // the first input is the angle value
+          target_dist = 0.0;
           target = input;
-//          dist = 0;
-//          angle = 0;
-//          count = 0;   
-//          flag = true;
           Serial.print("set degree: ");
           Serial.println(target);
-        } else {
+        } else { // the second input is the distance value
           target_dist = input;
-          target = 0;
-//          dist = 0;
-//          angle = 0;
-//          count = 0;   
-//          flag = true;
+          target = 0.0;
           Serial.print("set distance: ");
           Serial.println(target_dist);
         }
@@ -107,10 +95,11 @@ void loop() {
 
   
   if (recv == 0) {
-        float tar = -1 * (float)target/180.0 * PI / 2.0;
-        if (tar < 0) {
+        float tar_l = -1 * (float)target/180.0 * PI / 1.85;
+        float tar_r = -1 * (float)target/180.0 * PI / 2.1;
+        if (tar_l < 0) {
           Serial.println("turning left");
-          while (angle > tar) {
+          while (angle > tar_l) {
             velocity_linear = getLinearVelocity();
             velocity_angular = getAngularVelocity();
             applyPowerLeft(0.23);
@@ -119,15 +108,15 @@ void loop() {
               curr_time = micros();
               angle += (velocity_angular)*((float)(curr_time - prev_time)/1000000.0);
               prev_time = curr_time;
-              end_time = checkEncodersZeroVelocity();
           }
-           count++;  
+           count++;
+           end_time = checkEncodersZeroVelocity(); 
          }
          angle = 0;
          count = 0;
-        } else if (tar > 0) {
+        } else if (tar_r > 0) {
             Serial.println("turning right");
-          while (angle < tar) {
+          while (angle < tar_r) {
             velocity_linear = getLinearVelocity();
             velocity_angular = getAngularVelocity();
             applyPowerLeft(-0.2);
@@ -136,9 +125,9 @@ void loop() {
               curr_time = micros();
               angle += (velocity_angular)*((float)(curr_time - prev_time)/1000000.0);
               prev_time = curr_time;
-              end_time = checkEncodersZeroVelocity();
             }
             count++;
+            end_time = checkEncodersZeroVelocity();
          }
          angle = 0;
          count = 0;
@@ -155,7 +144,7 @@ void loop() {
           u_left = k_left * velocity_angular;
           u_right = k_right * (-1) * velocity_angular;  
           
-          float left_power = 0.2;
+          float left_power = 0.21;
           float right_power = 0.2;
           
           applyPowerLeft(left_power + u_left);
@@ -164,22 +153,22 @@ void loop() {
           // Print debug info every 500 loops
           if (count % 50 == 0) {
             curr_time = micros();
-            dist = dist + (velocity_linear / 100.0)*((float)(curr_time - prev_time)/1000000.0);
+            dist = dist + (velocity_linear / 10.0)*((float)(curr_time - prev_time)/1000000.0);
             prev_time = curr_time;
           }
-      count++;
+          count++;
     
-      end_time = checkEncodersZeroVelocity();
+          end_time = checkEncodersZeroVelocity();
+      }
+      count = 0;
+      dist = 0;
     }
-     count = 0;
-     dist = 0;
-  }
   }
   
     target= 0;
     target_dist = 0;
-  if (end_time != 0 && flag) {
-//    Serial.print("Time interval: ");
+  if (end_time != 0 && flag && input != 0) {
+    Serial.print("Time interval: ");
     Serial.print((end_time - start_time));
     Serial.print("\n");
     flag = false;
